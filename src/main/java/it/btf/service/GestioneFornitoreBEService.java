fa967df8eee76851f4a2d7cb750b97cf6d856d3b
Ale Beni <beninatiale@gmail.com>
2018-07-02 19:10:24 +0200

    servizio e repository per coordinate

--- src/main/java/it/btf/service/GestioneFornitoreBEService.java
+++ src/main/java/it/btf/service/GestioneFornitoreBEService.java
@@ -4,6 +4,7 @@ import it.btf.dto.FornitoreDTO;
 import it.btf.dto.ServizioDTO;
 import it.btf.interf.GestioneFornitoreBE;
 import it.btf.model.Fornitore;
+import it.btf.model.Guest;
 import it.btf.model.Servizio;
 import it.btf.repository.FornitoreRepository;
 import org.dozer.DozerBeanMapper;
@@ -44,6 +45,36 @@ public class GestioneFornitoreBEService implements GestioneFornitoreBE {
         }
         return fornitoriDTOS;
     }
+/*
+    @Override
+    public List<FornitoreDTO> loadAllByComune(String comune) {
+        List<FornitoreDTO> fornitoreDTOS = new ArrayList<FornitoreDTO>();
+        List<Fornitore> fornitori = fornitoreRepository.findAllByComune(comune);
+
+        for(Fornitore f:fornitori){
+         FornitoreDTO forn=  new DozerBeanMapper().map(f, FornitoreDTO.class);
+         fornitoreDTOS.add(forn);
+        }
+        return fornitoreDTOS;
+    }
+*/
+@Override
+public List<FornitoreDTO> loadAllGuestFornByComune(String comune) {
+    List<FornitoreDTO> fornitoreDTOS = new ArrayList<FornitoreDTO>();
+    List<Fornitore> fornitori = fornitoreRepository.findAllByComune(comune);
 
 
+    for(Fornitore f:fornitori){
+        FornitoreDTO forn=  new FornitoreDTO();
+        forn.setComune(f.getVia().getPaese());
+        forn.setNome(f.getNome());
+        forn.setDescrizione(f.getDescrizione());
+        forn.setNomeProfessione(f.getProfessione().getNome());
+        forn.setLatit(f.getVia().getLatit());
+        forn.setLongit(f.getVia().getLongit());
+
+        fornitoreDTOS.add(forn);
+    }
+    return fornitoreDTOS;
+}
 }
